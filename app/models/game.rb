a7090b4909aafd1cad56548695546933b3cc8f44
Meg Tuls <meglktuls@gmail.com>
2015-09-15 17:19:35 -0500

    Add sorting method to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,12 +8,12 @@ class Game < ActiveRecord::Base
   def avg_user_rating
     rating_sum = 0
     if self.ratings.empty?
-      return "no ratings"
+      "no ratings"
     else
       self.ratings.each do |rating|
         rating_sum += rating.stars
       end
-      return average_rating = rating_sum/self.ratings.length
+      average_rating = rating_sum/self.ratings.length
     end
   end
 
@@ -21,6 +21,12 @@ class Game < ActiveRecord::Base
     img_file = self.name.gsub(" ", "_")
     img_file = "/" + img_file + ".jpeg"
     img_file
+  end
+
+  def self.order_by_user_friend_rating
+    not_rated = all.select { |game| game.ratings.empty? }
+    user_rated = (all - not_rated).sort { |low, high| high.avg_user_rating <=> low.avg_user_rating }
+    user_sorted = user_rated.concat(not_rated)
   end
 
   def self.search(query)

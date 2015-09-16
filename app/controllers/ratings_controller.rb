f84a3b990364460365cd764589df10058a9a12b2
alita42 <alita42@gmail.com>
2015-09-15 19:42:52 -0500

    adding star rating system

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,21 +4,16 @@ class RatingsController < ApplicationController
   end
   def create
     @game = Game.find(params[:game_id])
-    p params
     @rating = Rating.new(rating_params)
     @game.ratings << @rating
     current_user.ratings << @rating
     redirect_to @game
   end
 
-
-
   private
 
   def rating_params
     params.require(:rating).permit(:stars, :comment)
   end
-
-
 
 end

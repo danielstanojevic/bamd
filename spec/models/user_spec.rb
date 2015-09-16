4897bf78227eddb5714af582cfa6fd7d745e5771
Alex Green & Blanche Hollingshead <pair+algreen88.alita42@devbootcamp.com>
2015-09-16 14:02:58 -0500

    Add initial user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,17 @@
+require 'rails_helper'
 
+describe User do
+
+  let (:user) { User.create!(username: "tester", password: "boppy") }
+  let (:user2) { User.create!(username: "tester2", password: "boppy") }
+  let (:user3) { User.create!(username: "tester3", password: "boppy") }
+  let (:pending_friendship) { Friendship.create!(from_id: 1, to_id: 2, confirmed: nil) }
+  let (:confirmed_friendship) { Friendship.create!(from_id: user.id, to_id: 3, confirmed: true) }
+
+  describe "#confirmed_friendships" do
+    it "returns confirmed friendships" do
+      expect(user.confirmed_friendships).to eq([confirmed_friendship])
+    end
+  end
+
+end
